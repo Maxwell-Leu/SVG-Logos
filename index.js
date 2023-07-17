21c9b5b3bd1d2c05fc034ee1ad36869b144a13ae
Maxwell L <mleuthner2002@gmail.com>
2023-07-17 15:04:46 -0500

    questions

---
+++ index.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the text in the logo?',
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: 'What is the text color?',
+      name: 'textColor',
+    },
+    {
+      type: 'input',
+      message: 'What is the color of you logo?',
+      name: 'logoColor',
+    },
+    {
+        type: 'input',
+        message: 'What is the of your logo?',
+        name: 'shape',
+    },
+  ])
+  .then((response) =>
+    console.log('works')
+  );
